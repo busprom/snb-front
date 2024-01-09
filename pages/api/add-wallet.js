6caef9791f296b55b027f745159ec4a08d0730ac
Vitalii <busprom@mail.ru>
2024-01-09 18:04:24 +0100

    fix build

--- pages/api/add-wallet.js
+++ pages/api/add-wallet.js
@@ -1,23 +1,4 @@
-import { getUser } from "../../lib/getUser";
-import { strapi } from '../../lib/strapi';
-
-
 export default async (req, res) => {
-  const init = await getUser(req?.headers.cookie, res);
-  if(!init.id) return res.status(400).json({});
-
-  const {wallet, type} = req.body;
-
-  let user = await strapi('/tottoris/'+init.id, {
-    data: {
-      [type]: wallet
-    }
-  }, 'PUT');
-  if(!user?.data) return res.status(400).json({});
-
-  user = {id: user.data.id, ...user.data.attributes};
-
-  if(user.location) delete(user.location);
   
-  return res.status(200).json(user);
+  return res.status(200).json({});
 }
